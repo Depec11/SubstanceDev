0f5e8248322139fa986538b1d4ca26dfbdb3f23d
depec_o_djemi <depec_o_djemi@noreply.gitcode.com>
2026-07-24 21:47:21 +0800

    清除测试内容

--- Test/Application.cs
+++ Test/Application.cs
@@ -4,4 +4,8 @@ namespace Test;
 
 public class Application(WindowOptions? options = null) : Substance.Application(options)
 {
+    protected override void OnCreatedOverride()
+    {
+        base.OnCreatedOverride();
+    }
 }
